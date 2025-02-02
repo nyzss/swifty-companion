a7922449900596f72d11fe6903a867dbc72d6159
nyzs <oknakoca@gmail.com>
2025-02-02 13:53:48 +0100

    chore: run reset-project

---
+++ app/index.tsx
@@ -0,0 +1,29 @@
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+    return (
+        <View
+            style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
+            <Text style={styles.title}>This is a title</Text>
+            <Link href={"/information"} style={styles.link}>
+                Go to information
+            </Link>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    title: {
+        fontSize: 80,
+        fontWeight: "bold",
+    },
+    link: {
+        color: "blue",
+    },
+});
---
+++ app/information.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Information() {
+    return (
+        <View>
+            <Text>hello world hahaha</Text>
+        </View>
+    );
+}
